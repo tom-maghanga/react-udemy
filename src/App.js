7afccb17bc1ca99f693b7fb14138277a16775f42
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2024-04-07 09:14:15 +0300

    used useState to add - Add & Sub & Reset functionalities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ count, setCount] = useState(0);
+
+  function handleAdd(){
+    setCount(count +1);
+  }
+
+  function handleSub() {
+    setCount(count - 1);
+  }
+
+  function handleReset() {
+    setCount(0);
+  }
+
   return (
     <div className="App">
-     
+      <div className='box'>
+        <p>{count}</p>
+        <button onClick={handleAdd} className='add'>Add</button>
+        <button onClick={handleSub} className='sub'>Sub</button>
+        <button onClick={handleReset} className='reset'>Reset</button>
+
+
+      </div>
     </div>
   );
 }
